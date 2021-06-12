2d803fceab31195ff6fa8ad209b2fe0438f130cf
Niklas Mohrin <dev@niklasmohrin.de>
2021-06-12 19:22:57 +0200

    kernel crate: Add `dbg` macro
    This macro mirrors the `std::dbg` macro. To view the output, you might
    have to set the log level to "info" using `dmesg -n 6`.

--- rust/kernel/print.rs
+++ rust/kernel/print.rs
@@ -411,6 +411,33 @@ macro_rules! pr_cont (
     )
 );
 
+/// Equivalent to `std::dbg`, but uses `pr_info`.
+#[macro_export]
+macro_rules! dbg {
+    // Comment from original implementation:
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        $crate::pr_info!("[{}:{}]", core::file!(), core::line!());
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::pr_info!("[{}:{}] {} = {:#?}",
+                    core::file!(), core::line!(), core::stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
+
 /// Extension trait for the `expect` method on `Option` and `Result` that prints an error message
 /// with `printk` before panicking.
 pub trait ExpectK<T> {
