3275f7af0d24f83d50a978f2efae296fabdc9343
Niklas Mohrin <dev@niklasmohrin.de>
2021-09-18 22:13:18 +0200

    actually, this stuff doesn't really belong into this PR branch, we can still git-checkout from the commit before this one

--- rust/kernel/fs/mod.rs
+++ rust/kernel/fs/mod.rs
@@ -1,13 +1,5 @@
 pub mod address_space;
-pub mod file_system;
-// pub mod address_space_operations;
 pub mod dentry;
 pub mod inode;
-// pub mod inode_operations;
 pub mod kiocb;
-pub mod libfs_functions;
-// pub mod object_wrapper;
 pub mod super_block;
-// pub mod super_operations;
-
-pub use file_system::*;
